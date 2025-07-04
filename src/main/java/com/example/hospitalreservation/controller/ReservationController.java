42a3589b4665220788ca86fc9650719a79d88094
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2025-07-04 14:42:04 +0900

    update: API Mapping url 수정

--- src/main/java/com/example/hospitalreservation/controller/ReservationController.java
+++ src/main/java/com/example/hospitalreservation/controller/ReservationController.java
@@ -4,6 +4,7 @@ import com.example.hospitalreservation.service.ReservationService;
 import java.time.LocalDateTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 // TODO : 컨트롤러에 필요한 어노테이션을 작성해주세요.
 // TODO : 요청 경로는 templates를 참고하여 작성해주세요.
 @Controller
-@RequestMapping("/reservations")
+@RequestMapping
 public class ReservationController {
 
     // TODO : 주입 받아야 할 객체를 설정해주세요.
@@ -25,7 +26,7 @@ public class ReservationController {
     }
 
     // TODO : 필요한 어노테이션을 작성해주세요.
-    @GetMapping
+    @GetMapping("/reservations")
     public String getReservations(Model model) {
         // TODO : 예약 메인 페이지를 가져오는 코드를 작성해주세요.
         model.addAttribute("reservations", reservationService.getAllReservations());
@@ -33,7 +34,7 @@ public class ReservationController {
     }
 
     // TODO : 필요한 어노테이션을 작성해주세요.
-    @GetMapping("/new")
+    @GetMapping("/reservations/new")
     public String showReservationForm() {
         // TODO : 예약하기 페이지를 가져오는 코드를 작성해주세요.
         return "reservation_form";
@@ -41,7 +42,7 @@ public class ReservationController {
 
     // TODO : 필요한 어노테이션을 작성해주세요.
     @ResponseBody
-    @PostMapping
+    @PostMapping("/api/reservations")
     public String createReservation(@RequestParam Long doctorId, @RequestParam Long patientId, @RequestParam LocalDateTime reservationTime) {
         // TODO : 예약을 진행하는 코드를 작성해주세요.
         reservationService.createReservation(doctorId, patientId, reservationTime);
@@ -49,7 +50,7 @@ public class ReservationController {
     }
 
     // TODO : 필요한 어노테이션을 작성해주세요.
-    @PostMapping("/delete/{id}")
+    @DeleteMapping("/api/reservations/{id}")
     @ResponseBody
     public String cancelReservation(@PathVariable Long id) {
         // TODO : 예약을 취소하는 코드를 작성해주세요.
